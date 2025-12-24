358c3b51776c65e79d504e384a9ff755dcf2c927
Duván González <duglas@MBP-de-Duvan.bbrouter>
2025-12-24 16:31:01 -0500

    (server-gateway) Hacer configuración inicial para acceder a 'msvc-course'

---
+++ infrastructure/server-gateway/src/test/java/co/duvan/gateway/app/ServerGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package co.duvan.gateway.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
