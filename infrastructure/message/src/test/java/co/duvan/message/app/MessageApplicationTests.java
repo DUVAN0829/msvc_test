9769f95a056373bd16d31ad9b0e04fa8449bedad
DUVAN0829 <duvthan067@gmail.com>
2026-01-16 16:33:25 -0500

    Añade msvc (message-msvc)

---
+++ infrastructure/message/src/test/java/co/duvan/message/app/MessageApplicationTests.java
@@ -0,0 +1,13 @@
+package co.duvan.message.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
